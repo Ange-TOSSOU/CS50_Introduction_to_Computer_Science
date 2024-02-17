bfa2e4de707d11b8b166231e05ac0cf705e16170
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-02-17 11:11:21 +0000

    Sat, Feb 17, 2024, 12:11 PM +01:00

--- meow.c
+++ meow.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-void meow(void);
+void meow(int n);
 
 int main(void)
 {
-    for (int i = 0; i < 3; i++)
-        meow();
+    meow(30000);
 }
 
-void meow(void)
+void meow(int n)
 {
-    printf("meow\n");
+    for (int i = 0; i < n; i++)
+        printf("meow\n");
 }
