19b0128be6168c0a0bdea4428fd569424718a720
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-02-17 12:21:48 +0000

    Sat, Feb 17, 2024, 1:21 PM +01:00

--- mario.c
+++ mario.c
@@ -3,7 +3,13 @@
 
 int main(void)
 {
-    const int n = get_int("Size: ");
+    int n;
+
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while(n < 1);
 
     for (int i = 0; i < n; i++)
     {
