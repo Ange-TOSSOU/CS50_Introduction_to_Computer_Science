49fab3a9d798edc8dbe2d1e050041324bea473e2
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-02-20 02:33:09 +0000

    Tue, Feb 20, 2024, 3:33 AM +01:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,31 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void print_left(int h, int n);
+void print_middle(void);
+void print_right(int h, int n);
+
 int main(void)
 {
-    int height = get_int("Height: ");
+    int height = 0;
+    do {
+        height = get_int("Height: ");
+    }while(height < 1 || height > 8);
+}
+
+void print_left(int h, int n)
+{
+    for (int i = 0; i < h-n; i++)
+        printf(" ");
+
+    for (int i = 0; i < n; i++)
+        printf("#");
+}
+
+void print_middle(void);
+
+void print_right(int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("#");
 }
