73e1765940dee41a6de98306fd99b78a6b77f128
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-02-20 03:10:23 +0000

    Tue, Feb 20, 2024, 4:10 AM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 
 int number_of_digits(long n);
+void print_type_of_card(long n);
 
 int main(void)
 {
     int number = get_long("Number: ");
+    int digits = number_of_digits(number);
 
-    int 
+
 }
 
 int number_of_digits(long n)
@@ -22,4 +24,13 @@ int number_of_digits(long n)
     while (n != 0);
 
     return digits;
+}
+
+void print_type_of_card(long n)
+{
+    int digits = number_of_digits(n);
+
+    if (digits == 15)
+        printf("AMEX\n");
+    else if (digits == 15)
 }
