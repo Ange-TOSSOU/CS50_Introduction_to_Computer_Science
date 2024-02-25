9ca73ae1ecf3a9b2747c73bf9a39b1a570448d33
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-02-25 06:11:51 +0000

    Sun, Feb 25, 2024, 7:11 AM +01:00

--- scores.c
+++ scores.c
@@ -1,9 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
+const int N = 3;
+
+float average(int length, int array[]);
+
 int main(void)
 {
-    const int N = 3;
     int scores[N];
 
     for (int i = 0; i < N; i++)
@@ -11,5 +14,10 @@ int main(void)
         scores[i] = get_int("Score: ");
     }
 
-    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / (float) N);
+    printf("Average: %f\n", average(N, scores));
+}
+
+float average(int length, int array[])
+{
+    for (int i = 0; i < length; i++)
 }
