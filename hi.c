a6768a614a115c98ea89501f77357862e2cbb8b5
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-02-25 06:39:14 +0000

    Sun, Feb 25, 2024, 7:39 AM +01:00

--- hi.c
+++ hi.c
@@ -1,10 +1,9 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    char c1 = 'H';
-    char c2 = 'I';
-    char c3 = '!';
+    String s = "HI!";
 
-    printf("%i %i %i\n", c1, c2, c3);
+    printf("%c%c%c\n", s[0], s[1], s[2]);
 }
