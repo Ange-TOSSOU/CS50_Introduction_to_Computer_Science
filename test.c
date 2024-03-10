88c5480b3206da028ae4c1de0cd7315256fcacd6
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-03-10 13:38:35 +0000

    Sun, Mar 10, 2024, 2:38 PM +01:00

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string name;
+
+    name = "Thor";
+    printf("Name: %s\n", name);
+
+    name = "Flash";
+    printf("Name: %s\n", name);
+
+    return 0;
+}
