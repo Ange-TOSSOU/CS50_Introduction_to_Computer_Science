ccbd0aea75858675ee38963395a4096245baf725
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-03-10 14:51:46 +0000

    Sun, Mar 10, 2024, 3:51 PM +01:00

--- iteration.c
+++ iteration.c
@@ -14,5 +14,12 @@ int main(void)
 
 void draw(int n)
 {
-    for ()
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
