3266fdf5fb34a6350f982e6ffea9ea12387c78de
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-03-12 17:23:45 +0000

    Tue, Mar 12, 2024, 6:23 PM +01:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -65,13 +65,21 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, candidates[i].name) == 0)
+        {
+            return true;
+        }
+    }
     return false;
 }
 
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+    // Find the maximum of candidate's votes
+    int max = 0;
+    for (int )
     return;
 }
