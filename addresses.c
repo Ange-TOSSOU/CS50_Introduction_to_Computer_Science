868326dcb29d9182070a709a8b1cdd77416af5c5
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-03-16 06:25:39 +0000

    Sat, Mar 16, 2024, 7:25 AM +01:00

--- addresses.c
+++ addresses.c
@@ -3,9 +3,7 @@
 int main(void)
 {
     char *s = "HI!";
-    printf("%p\n", s);
-    printf("%p\n", &s[0]);
-    printf("%p\n", &s[1]);
-    printf("%p\n", &s[2]);
-    printf("%p\n", &s[3]);
+    printf("%c", s[0]);
+    printf("%c", s[1]);
+    printf("%c\n", s[2]);
 }
