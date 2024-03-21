60e377c540a53fcd1aac6144d8d55bcb6842318b
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-03-21 22:01:23 +0000

    Thu, Mar 21, 2024, 11:01 PM +01:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -116,24 +116,34 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE mean;
     const int n = 3;
     int kernel_x[n][n] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
     int kernel_y[n][n] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
 
     for (int i = 0; i < height; i++)
     {
-        height_c = i + 1;
         for (int j = 0; j < width; j++)
         {
+            mean.rgbtBlue = mean.rgbtGreen = mean.rgbtRed = 0;
             for (int k = 0; k < n ; k++)
             {
                 for (int l = 0; l < n; l++)
                 {
                     if (0 <= k && k < height && 0 <= l && l < width)
                     {
+                        mean.rgbtBlue += image[k][l].rgbtBlue;
+                        mean.rgbtGreen += image[k][l].rgbtGreen;
+                        mean.rgbtRed += image[k][l].rgbtRed;
+                        n++;
                     }
                 }
             }
+            mean.rgbtBlue = (BYTE)lrint((double)mean.rgbtBlue / n);
+            mean.rgbtGreen = (BYTE)lrint((double)mean.rgbtGreen / n);
+            mean.rgbtRed = (BYTE)lrint((double)mean.rgbtRed / n);
+
+            image[i][j] = mean;
         }
     }
 }
