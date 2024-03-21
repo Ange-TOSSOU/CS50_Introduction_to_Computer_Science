4d941eb567a71c3b0d4b74795ba9a3b31971f69f
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-03-21 21:26:36 +0000

    Thu, Mar 21, 2024, 10:26 PM +01:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -37,10 +37,12 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     RGBTRIPLE(*image_blur)[width] = calloc(height, width * sizeof(RGBTRIPLE));
     RGBTRIPLE mean;
     int height_c, width_c;
+    int n;
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
+            n = 0;
             mean.rgbtBlue = mean.rgbtGreen = mean.rgbtRed = 0;
             height_c = i + 1;
             width_c = j + 1;
@@ -52,13 +54,24 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     {
                         mean.rgbtBlue += image[k][l].rgbtBlue;
                         mean.rgbtGreen += image[k][l].rgbtGreen;
-                        mean.rgbtRed = image[k][l].rgbtRed;
+                        mean.rgbtRed += image[k][l].rgbtRed;
+                        n++;
                     }
                 }
             }
-            mean.rgbtBlue += image[k][l].rgbtBlue;
-            mean.rgbtGreen += image[k][l].rgbtGreen;
-            mean.rgbtRed = image[k][l].rgbtRed;
+            mean.rgbtBlue = (BYTE)lrint((double)mean.rgbtBlue / n);
+            mean.rgbtGreen = (BYTE)lrint((double)mean.rgbtGreen / n);
+            mean.rgbtRed = (BYTE)lrint((double)mean.rgbtRed / n);
+
+            image_blur[i][j] = mean;
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = image_blur[i][j];
         }
     }
 }
