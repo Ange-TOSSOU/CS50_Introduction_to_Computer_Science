186e3dd56a9afa0764438722556b1945a48acd88
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-03-21 09:22:30 +0000

    Thu, Mar 21, 2024, 10:22 AM +01:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -18,11 +18,13 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    double mean;
-    for (int i = 0; i < height; i++)
+    RGBTRIPLE tmp;
+    int lim = width / 2;
+    for (int i = 0; i < lim; i++)
     {
         for (int j = 0; j < width; j++)
         {
+            tmp =
             mean = image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed;
             mean /= 3;
             image[i][j].rgbtBlue = image[i][j].rgbtGreen = image[i][j].rgbtRed = (BYTE)lrint(mean);
