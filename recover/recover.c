d9dfac5a5adc2a86adbed02680b581e33d4097fb
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-03-24 08:37:55 +0000

    Sun, Mar 24, 2024, 9:37 AM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    // Accept a single command-line argument
+    if (argc != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
+
+    // Open the memory card
+    FILE *memory_card = fopen("card.raw", "r");
+    if (memory_card == NULL)
+    {
+        return 1;
+    }
+
+    // While there's still data left to read from the memory card
+
+        // Create JPEGs from the data
+
+    fclose(memory_card);
 }
