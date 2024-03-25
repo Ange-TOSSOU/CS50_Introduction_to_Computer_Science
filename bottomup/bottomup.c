1ee3ef7b57f1298be7fd5b240e84c656806b9df3
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-03-25 06:37:01 +0000

    Mon, Mar 25, 2024, 7:37 AM +01:00

--- bottomup/bottomup.c
+++ bottomup/bottomup.c
@@ -53,6 +53,7 @@ int main(int argc, char *argv[])
         return 4;
     }
 
+    printf("%i\n", bi.biHeight);
     // Write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
 
