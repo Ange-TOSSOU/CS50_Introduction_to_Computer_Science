fe4f04cc7c26aecc9b5c5a787ea2fb750aea8278
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-03-27 21:37:00 +0000

    Wed, Mar 27, 2024, 10:37 PM +01:00

--- list.c
+++ list.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+typedef struct node
+{
+    int number;
+    struct node *next;
+}node;
 
+int main(int argc, char *argv[])
+{
+    node *list = NULL;
+
+    for (int i = 1; i )
+}
