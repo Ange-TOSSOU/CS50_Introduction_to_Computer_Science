debfa82a80b07ff0d83bf8b89040c5b85591e548
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-03-31 12:39:15 +0000

    Sun, Mar 31, 2024, 2:39 PM +02:00

--- queue/arrays_based.c
+++ queue/arrays_based.c
@@ -32,7 +32,7 @@ int enqueue(Queue *q, int value)
         return 0;
     }
 
-    q->array[q->front + q->size] = value;
+    q->array[(q->front + q->size) % q->capacity] = value;
     q->size++;
 
     return 1;
