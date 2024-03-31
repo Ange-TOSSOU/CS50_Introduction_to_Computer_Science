9782783e2816ad6f761033c67775e2c8598c2331
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-03-31 07:45:56 +0000

    Sun, Mar 31, 2024, 9:45 AM +02:00

---
+++ stacks/lists_based.c
@@ -0,0 +1,57 @@
+#include "singly.h"
+
+Node *create(int value)
+{
+    Node *n = malloc(sizeof(Node));
+
+    if (n != NULL)
+    {
+        n->value = value;
+        n->next = NULL;
+    }
+
+    return n;
+}
+
+int find(Node *head, int value)
+{
+    Node *current = head;
+
+    while (current != NULL)
+    {
+        if (current->value == value)
+        {
+            return 1;
+        }
+        current = current->next;
+    }
+
+    return 0;
+}
+
+Node *insert(Node *head, int value)
+{
+    Node *n = create(value);
+
+    if (n == NULL)
+    {
+        return head;
+    }
+
+    n->next = head;
+
+    return n;
+}
+
+Node *destroy(Node *head)
+{
+    if (head == NULL)
+    {
+        return NULL;
+    }
+
+    destroy(head->next);
+    free(head);
+
+    return NULL;
+}
