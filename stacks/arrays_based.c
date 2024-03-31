e75ea3388fc6e0ca375843aea3836d66a167a33d
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-03-31 07:41:16 +0000

    Sun, Mar 31, 2024, 9:41 AM +02:00

---
+++ stacks/arrays_based.c
@@ -0,0 +1,48 @@
+#include "arrays_based.h"
+
+Stack *create(int capacity)
+{
+    if (capacity <= 0)
+    {
+        return NULL;
+    }
+
+    Stack *s = malloc(sizeof(Stack));
+    if (s == NULL)
+    {
+        return NULL;
+    }
+
+    s->array = malloc(capacity * sizeof(int));
+    if (s->array == NULL)
+    {
+        free(s);
+        return NULL;
+    }
+    s->capacity = capacity;
+    s->top = 0;
+
+    return s;
+}
+
+int push(Stack *s, int value)
+{
+    if (s == NULL || s->top + 1 > CAPACITY)
+    {
+        return 0;
+    }
+
+    s->array[s->top++] = value;
+
+    return 1;
+}
+
+int pop(Stack *s)
+{
+    if (s == NULL || s->top == 0)
+    {
+        return 0;
+    }
+
+    return s->array[s->top--];
+}
--- stacks/arrays_based.h
+++ stacks/arrays_based.h
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-#define CAPACITY 20
-
 typedef struct
 {
-    int array[CAPACITY];
+    int *array;
+    int capacity;
     int top;
 } Stack;
 
-void push(Stack *s, int value);
+Stack *create(int capacity);
+int push(Stack *s, int value);
 int pop(Stack *s);
