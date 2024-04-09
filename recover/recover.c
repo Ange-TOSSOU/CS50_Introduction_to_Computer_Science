f4663b45c327682146f75240c45dd91b3b1b98d3
Ange-TOSSOU <126680380+Ange-TOSSOU@users.noreply.github.com>
2024-04-09 17:55:56 +0000

    Tue, Apr 9, 2024, 7:55 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
     // While there's still data left to read from the memory card
     while (fread(buffer, sizeof(BYTE), JPEG_BLOCK_SIZE, card) == sizeof(BYTE) * JPEG_BLOCK_SIZE)
     {
-        // Verify if it is a beginning of a JPEG file
+        // Verify if it is probably a beginning of a JPEG file
         if (is_jpeg(buffer))
         {
             // Close an existing opened JPEG file
